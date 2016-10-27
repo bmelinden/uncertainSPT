1fbf9c15aedda1236ce1f4d06daecbace9289e7c
Martin Lindén <bmelinden@gmail.com>
2016-10-27 15:01:34 +0200

    removed external cdf fun and errbar

--- tools/empiric_cdf.m
+++
@@ -1,8 +0,0 @@
-function [x,cdf]=empiric_cdf(x)
-% [x,cdf]=empiric_cdf(x)
-% compute the empirical cumulative distribution function of the
-% observations x.
-
-x=reshape(sort(x(:)),1,numel(x));
-cdf=(1:numel(x))/numel(x);
-
