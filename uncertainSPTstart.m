4a7b0f3e5a8297eb34dc6e27be3e15838e0231f8
Martin Lindén <bmelinden@gmail.com>
2017-01-27 10:54:55 +0100

    better path setup

--- uncertainSPTstart.m
+++ uncertainSPTstart.m
@@ -1,7 +1,11 @@
 function uncertainSPTstart()
 % add uncertainSPT to the matlab path
 
-mfilepath=fileparts(mfilename('fullpath'));
-addpath(genpath(mfilepath))
-rmpath(genpath(fullfile(mfilepath,'.git')))
+dir0=fileparts(mfilename('fullpath'));% path to this file, even if called from other folder
+addpath(dir0)
+addpath(fullfile(dir0,'HMMcore'))
+addpath(fullfile(dir0,'tools'))
+disp('Added local uncertainSPT paths from')
+disp(dir0)
+disp('-----------------------')
 
